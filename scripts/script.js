36f573378b0cdb302800a2e3cf1b180c1029c17b
zdimall <35106237+zdimall@users.noreply.github.com>
2018-08-27 09:18:12 +0200

    Delete script.js

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-var seznam = ["Abatyše","Adolf","Angola","Afrika","Almužna",
-              "Arcibiskup","Argentina","Atlas", "Audio", "Aurora"];
-
-for (var i = 0; i < seznam.length; i++) {
-    $(".datalistclass").append("<option value='" + seznam[i] + "'></option>");
-}
